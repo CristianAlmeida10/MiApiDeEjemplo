6a74417f58a09e73dbff0bcdc6e88fdf13af7495
CristianAlmeida10 <cdas2710@gmail.com>
2022-08-13 13:41:41 -0500

    Ejemplo ADO.NET

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiPrimeraAPI.Model;
+
+namespace MiPrimeraAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet(Name = "GetUsuarios")]
+        public List<Usuario> GetUsuario()
+        {
+            return new List<Usuario>();
+        }
+    }
+}
---
+++ Repository/UsuarioHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace MiPrimeraAPI.Repository
+{
+    public static class UsuarioHandler
+    {
+        public const string ConnectionString = "Server=localhost;Initial Catalog=SistemaGestion;Trusted_Conection=true";
+
+    }
+}
